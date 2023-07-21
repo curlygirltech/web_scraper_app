f0e05b2e752af6698d583ebb55e8ceb777feafb1
Joshua Morgan <jmorg0605@gmail.com>
2023-07-21 13:28:38 -0400

    cleared out spaces

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
   console.log(req);
-
   res.json({ message: "Data received", data: req.body });
 });
 
 const port = process.env.PORT || 5100;
+
 app.listen(port, () => {
   console.log(`server running on PORT ${port}....`);
 });
