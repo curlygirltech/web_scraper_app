e0134ffaa9b9f60343d12480cc4f3743234ba680
Joshua Morgan <jmorg0605@gmail.com>
2023-07-21 13:11:05 -0400

    added default values for gitignore, and two packages
    Morgan will just http requests as we develop.  Dotenv might not be needed, but just in case, I have added it

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/", (req, res) => {
+  console.log(req);
+
+  res.json({ message: "Data received", data: req.body });
+});
+
+app.listen(5100, () => {
+  console.log("server running....");
+});
