d25babf8de43a35c27be08f85d096a2ed999b856
Dina <dinagiacobbi@gmail.com>
2023-07-24 06:42:06 -0800

    Clean up files, add components folder, route folder, homepage button with css

---
+++ basketball-stats-app/components/home-page/Home.js
@@ -0,0 +1,13 @@
+import classes from './Home.module.css'
+
+const Home = () => {
+  return (
+    <div>
+      <button className={classes.button}>
+        Basket Ball Statistics
+      </button>
+    </div>
+  )
+}
+
+export default Home
