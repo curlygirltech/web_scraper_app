5579d7d7c0145d3312817738684c7620d1abbbcc
Joshua Morgan <jmorg0605@gmail.com>
2023-07-31 18:15:20 -0400

    changed static props function to server-side function
    I removed the component I had in earlier for testing purposes.

--- basketball-stats-app/pages/stats/index.js
+++ basketball-stats-app/pages/stats/index.js
@@ -21,10 +21,11 @@ export default statsindex
 import Scraper from "../../utils/scraper";
 
 export default function Stats(props) {
+  console.log(props.loadedData);
   return <div>Stats</div>;
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   let scraper = new Scraper();
   let data = await scraper.getStats();
 
