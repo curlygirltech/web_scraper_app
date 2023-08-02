886d6772182ecb2788945ee1ef0c837820baf20a
Dina <dinagiacobbi@gmail.com>
2023-08-02 08:16:56 -0800

    Add small change

--- basketball-stats-app/pages/index.js
+++ basketball-stats-app/pages/index.js
@@ -1,38 +1,32 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 //Landing Home page
 import { Fragment } from 'react'
 // import Link from 'next/link'
 
 import StatsItem from '@/components/StatsItem'
-=======
+
 // Landing Home page
 import { Fragment } from "react";
 import Link from "next/link";
->>>>>>> ae63e6d (configured puppeteer to run on server-side component)
-=======
+
 // Landing Home page
 import { Fragment } from "react";
 import Link from "next/link";
-=======
 //Landing Home page
 import { Fragment } from 'react'
 // import Link from 'next/link'
 
 import StatsItem from '@/components/StatsItem'
->>>>>>> 9eb9c77 (update components)
->>>>>>> 52ebe91 (update components)
+
 
 const HomePage = () => {
   return (
     <Fragment>
-<<<<<<< HEAD
-<<<<<<< HEAD
+
       <h1>Hello</h1>
       <StatsItem />
-=======
-=======
->>>>>>> 52ebe91 (update components)
+
+
       <h1>BASKETBALL STATISTICS HOME PAGE</h1>
       <ul>
         <li>
@@ -40,14 +34,10 @@ const HomePage = () => {
           <Link href="/api/route">Get the most current salary statistics</Link>
         </li>
       </ul>
-<<<<<<< HEAD
->>>>>>> ae63e6d (configured puppeteer to run on server-side component)
-=======
-=======
+
       <h1>Hello</h1>
       <StatsItem />
->>>>>>> 9eb9c77 (update components)
->>>>>>> 52ebe91 (update components)
+
     </Fragment>
   );
 };
